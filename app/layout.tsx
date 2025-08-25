d0cbbe2d2db1773110a3fc1ec675ba9b647c1078
Fahmin Mohammed <kkfahmin@gmail.com>
2025-08-25 17:26:24 +0530

    Update personal information and project details across multiple files

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,8 +29,8 @@ const robotoFlex = Roboto_Flex({
 });
 
 export const metadata: Metadata = {
-    title: 'Portfolio - Tajmirul Islam',
-    description: 'Personal portfolio of Tajmirul Islam',
+    title: 'Portfolio - Fahmin Md',
+    description: 'Personal portfolio of Fahmin Md',
 };
 
 export default function RootLayout({
--- lib/data.ts
+++ lib/data.ts
@@ -1,19 +1,19 @@
 import { IProject } from '@/types';
 
 export const GENERAL_INFO = {
-    email: 'tasmirolislam@gmail.com',
+    email: 'kkfahmin@gmail.com',
 
     emailSubject: "Let's collaborate on a project",
-    emailBody: 'Hi Tajmirul, I am reaching out to you because...',
+    emailBody: 'Hi Fahmin, I am reaching out to you because...',
 
     oldPortfolio: 'https://www.legacy.me.toinfinite.dev',
-    upworkProfile: 'https://www.upwork.com/freelancers/tajmirul',
+    upworkProfile: 'https://www.upwork.com/freelancers/',
 };
 
 export const SOCIAL_LINKS = [
-    { name: 'github', url: 'https://github.com/Tajmirul' },
-    { name: 'linkedin', url: 'https://www.linkedin.com/in/tajmirul' },
-    { name: 'facebook', url: 'https://www.facebook.com/tajmirul.2000' },
+    { name: 'github', url: 'https://github.com/fahmmin' },
+    { name: 'linkedin', url: 'https://www.linkedin.com/in/fahminmohammed' },
+    { name: 'facebook', url: 'https://www.facebook.com/' },
     { name: 'Old Version', url: GENERAL_INFO.oldPortfolio },
 ];
 
@@ -110,9 +110,9 @@ export const MY_STACK = {
 
 export const PROJECTS: IProject[] = [
     {
-        title: 'MTI Electronics',
-        slug: 'mti-electronics',
-        liveUrl: 'https://mti-electronics.vercel.app/',
+        title: 'Hex Oracle',
+        slug: 'hex-oracle',
+        liveUrl: 'https://hex-oracle.vercel.app/',
         year: 2025,
         description: `
       A complete agency portfolio platform built for MTI Electronics to showcase their services, blog content, and product offerings. <br/> <br/>
@@ -163,8 +163,8 @@ export const PROJECTS: IProject[] = [
         ],
     },
     {
-        title: 'Epikcart',
-        slug: 'epikcart',
+        title: 'DigiWill',
+        slug: 'digiwill',
         techStack: [
             'React',
             'Redux',
@@ -193,8 +193,8 @@ export const PROJECTS: IProject[] = [
         - Delivered a responsive, user-friendly interface in collaboration with the UI/UX designer.`,
     },
     {
-        title: 'Resume Roaster',
-        slug: 'resume-roaster',
+        title: 'Repo Rewards',
+        slug: 'repo-rewards',
         techStack: [
             'GPT-4',
             'Next.js',
@@ -209,18 +209,18 @@ export const PROJECTS: IProject[] = [
             '/projects/images/resume-roaster-2.png',
             '/projects/images/resume-roaster-3.png',
         ],
-        liveUrl: 'https://resume-roaster.vercel.app/',
+        liveUrl: 'https://repo-rewards.vercel.app/',
         year: 2023,
         description:
-            'Resume Roaster is a web application designed to provide tailored resume feedback and professional writing services. Built with Next.js, PostgreSQL, Prisma, and Tailwind CSS, it integrates GPT-4 for AI-powered recommendations. The platform also includes peer-to-peer reviews with a points-based system, fostering a collaborative and engaging experience. Targeting freshers, experienced professionals, and programmers, it helps optimize resumes for job-specific success.',
+            'Repo Rewards is a web application designed to provide tailored resume feedback and professional writing services. Built with Next.js, PostgreSQL, Prisma, and Tailwind CSS, it integrates GPT-4 for AI-powered recommendations. The platform also includes peer-to-peer reviews with a points-based system, fostering a collaborative and engaging experience. Targeting freshers, experienced professionals, and programmers, it helps optimize resumes for job-specific success.',
         role: `As the sole developer and business owner, I:<br/>
         - Designed and developed the platform end-to-end using Next.js, PostgreSQL, Prisma, and Tailwind CSS.<br/>
         - Integrated GPT-4 for AI-driven feedback and insights.<br/>
         - Implemented complex SQL queries, including one to identify the top two resumes based on user points.`,
     },
     {
-        title: 'Real Estate',
-        slug: 'property-pro',
+        title: 'NFT Fractionalizer',
+        slug: 'nft-fractionalizer',
         techStack: [
             'React.js',
             'Redux',
@@ -235,7 +235,7 @@ export const PROJECTS: IProject[] = [
             '/projects/images/property-pro-2.png',
             '/projects/images/property-pro-3.png',
         ],
-        liveUrl: 'https://demo.propertypro.siphertech.com/',
+        liveUrl: 'https://demo.nft-fractionalizer.siphertech.com/',
         year: 2023,
         description:
             'PropertyPro is a real estate management platform offering users a seamless experience to explore, manage, and view property listings. The application emphasizes accessibility and responsive design, ensuring a smooth interface across devices.',
@@ -245,64 +245,19 @@ export const PROJECTS: IProject[] = [
         - Implemented multi-language support with React i18n to cater to diverse audiences.<br/>
         - Enhanced user interaction with animations and transitions using Framer Motion.`,
     },
-    {
-        title: 'Consulting Finance',
-        slug: 'crenotive',
-        techStack: ['HTML', 'CSS & SCSS', 'Javascript', 'Bootstrap'],
-        thumbnail: '/projects/thumbnail/consulting-finance.jpg',
-        longThumbnail: '/projects/long/consulting-finance.jpg',
-        images: [
-            '/projects/images/consulting-finance-1.png',
-            '/projects/images/consulting-finance-2.png',
-            '/projects/images/consulting-finance-3.png',
-        ],
-        sourceCode: 'https://github.com/Tajmirul/crenotive',
-        liveUrl: 'https://crenotive.netlify.app/',
-        year: 2023,
-        description:
-            'I developed Crenotive, a portfolio website using Html, SASS, and jQuery to showcase services and expertise. The design focuses on responsive user experience and effective presentation of professional achievements.',
-        role: ``,
-    },
-    {
-        title: 'devLinks',
-        slug: 'devLinks',
-        techStack: ['Next.js', 'Formik', 'Drag & Drop', 'Tailwind CSS'],
-        thumbnail: '/projects/thumbnail/devLinks.jpg',
-        longThumbnail: '/projects/long/devLinks.jpg',
-        images: [
-            '/projects/images/devLinks-1.png',
-            '/projects/images/devLinks-2.png',
-            '/projects/images/devLinks-3.png',
-        ],
-        sourceCode: 'https://github.com/Tajmirul/devsLink',
-        liveUrl: 'https://devlinks-demo.vercel.app/auth/signin',
-        year: 2023,
-        description: `One of the most challenging projects in Frontend Mentor.<br/><br/>
-
-            I developed a LinkSharing App as part of the Frontend Mentor challenge, utilizing React, Redux, and Tailwind CSS to create a responsive and feature-rich platform. The app allows users to share, save, and explore links, with a focus on intuitive design and smooth navigation. Advanced state management ensures efficient data handling for user interactions.`,
-        role: ``,
-    },
+    
 ];
 
 export const MY_EXPERIENCE = [
     {
-        title: 'Software Engineer (Frontend)',
-        company: 'Strativ AB',
-        duration: 'Dec 2024 - Present',
+        title: 'Software Engineer',
+        company: 'RootVestors',
+        duration: 'Jun 2025 - Present',
     },
     {
-        title: 'Frontend Developer',
-        company: 'Epikcoders',
-        duration: 'Oct 2023 - Nov 2024',
+        title: 'Vice Head',
+        company: 'Dev Club IIITNR',
+        duration: 'Aug 2025 - Present',
     },
-    {
-        title: 'Frontend Engineer',
-        company: 'Anchorblock Technology',
-        duration: 'Oct 2022 - Sep 2023',
-    },
-    {
-        title: 'Frontend Developer (Part-time)',
-        company: 'Branex IT',
-        duration: 'Jan 2022 - Oct 2022',
-    },
+
 ];
